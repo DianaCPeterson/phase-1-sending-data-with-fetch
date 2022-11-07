47d9f648f025f842438a54e8c5c07a3daded3d39
Diana C Peterson <dpeter10@gmail.com>
2022-11-07 17:01:03 -0500

    commit tree 34b65e130dcc51d99e6c2f5ff33939fce1f09f8c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+const usersURL = `http://localhost:3000/users`
+// In`submitData`, write a valid POST request to`http://localhost:3000/users`
+// using`fetch()`.This request should include:
+
+// - The destination URL
+//     - Headers for 'Content-Type' and 'Accept', both set to 'application/json'
+//         - A body with the name and email passed in as arguments to`submitData`.These
+//   should be assigned to `name` and `email` keys within an object.This object
+//   should then be stringified.
+
+
+// Use a `then()` call to access the `Response` object and use its built-in
+// `json()` method to parse the contents of the `body` property. Use a _second_
+// `then()` to access this newly converted object. From this object, find the new
+// id and append this value to the DOM.
+// Use a `then()` call to access the `Response` object and use its built-in
+// `json()` method to parse the contents of the `body` property. Use a _second_
+// `then()` to access this newly converted object. From this object, find the new
+// id and append this value to the DOM.
+
+function submitData(name, email){
+    return fetch(usersURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name: name, email: email}),
+    })
+    .then(string => string.json())
+    .then(data => document.body.append(data.id))
+    .catch(error => document.body.append(error))
+}
+
+submitData()
+
